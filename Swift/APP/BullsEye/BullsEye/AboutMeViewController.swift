9a7a9b6848b4623323c6b32458efe7e3e0f9555b
winfred_zen <869158130@qq.com>
2018-03-02 21:00:48 +0800

    BullsEye

---
+++ Swift/APP/BullsEye/BullsEye/AboutMeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AboutMeViewController.swift
+//  BullsEye
+//
+//  Created by 王振 on 2018/3/1.
+//  Copyright © 2018年 wangzhen. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
